b81a600e76ae4d1c31aafb4cf5f58fbe594027a8
loehnertz <jakob2102@gmail.com>
2018-06-24 23:26:08 +0200

    Implemented the logic for the `AllVehicles` RPC

---
+++ src/services/telemetry/vehicles.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/go-redis/redis"
+	"github.com/loehnertz/toranos/src/commons"
+	"github.com/loehnertz/toranos/src/services/telemetry/proto"
+	"github.com/micro/go-log"
+)
+
+func retrieveAllVehicles(redisClient *redis.Client) (vehicles []*telemetry.AllVehiclesResponse_Vehicle) {
+	result, redisRetrievalError := redisClient.Get(RedisAllVehiclesKey).Result()
+
+	if redisRetrievalError != nil {
+		log.Log(redisRetrievalError)
+	} else {
+		var allVehicles []commons.Vehicle
+		jsonUnmarshalError := json.Unmarshal([]byte(result), &allVehicles)
+		if redisRetrievalError != nil {
+			log.Log(jsonUnmarshalError)
+		} else {
+			for i := range allVehicles {
+				vehicle := allVehicles[i]
+				vehicles = append(vehicles, &telemetry.AllVehiclesResponse_Vehicle{
+					VehicleId: vehicle.Id,
+					Location:  vehicle.Location,
+					Battery:   vehicle.Battery,
+				})
+			}
+		}
+	}
+
+	return
+}
