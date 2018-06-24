32fb3f21e3b523a62cd662b589ec2f9ea2c0287c
loehnertz <jakob2102@gmail.com>
2018-06-24 18:25:16 +0200

    Added the scaffold of the `FleetMonitor` service

---
+++ src/services/fleet-monitor/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/jasonlvhit/gocron"
+	"github.com/loehnertz/toranos/src/services/fleet-controller/proto"
+	"github.com/micro/go-micro"
+)
+
+var fleetController fleet_controller.FleetControllerService
+
+func main() {
+	service := micro.NewService(micro.Name("fleet-monitor"))
+	service.Init()
+
+	fleetController = fleet_controller.NewFleetControllerService("fleet-controller", service.Client())
+
+	gocron.Every(5).Seconds().Do(checkForExpiredReservations)
+	<-gocron.Start()
+}
