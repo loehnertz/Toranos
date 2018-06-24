62f18eb99fede96f1b4159db0d4bd3c2d62ace54
loehnertz <jakob2102@gmail.com>
2018-06-24 18:25:34 +0200

    Implemented the first task of the `FleetMonitor` service

---
+++ src/services/fleet-monitor/reservations.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/loehnertz/toranos/src/services/fleet-controller/proto"
+	"github.com/micro/go-log"
+	"time"
+)
+
+const ReservationTimeInSeconds = 900
+
+func checkForExpiredReservations() {
+	resReservations, errReservations := fleetController.RetrieveReservations(context.TODO(), &fleet_controller.Empty{})
+	if errReservations != nil {
+		log.Log(errReservations)
+	} else {
+		for i := range resReservations.Reservations {
+			reservation := resReservations.Reservations[i]
+			createdAt := time.Unix(reservation.CreatedAt, 0)
+
+			if time.Since(createdAt) > (ReservationTimeInSeconds * time.Second) {
+				resUnbook, errUnbook := fleetController.Unbook(context.TODO(), &fleet_controller.UnbookingRequest{
+					VehicleId:  reservation.Vehicle,
+					CustomerId: reservation.Customer,
+				})
+				if errUnbook != nil {
+					log.Log(errUnbook)
+				} else {
+					if resUnbook.Successful == true {
+						fmt.Printf("Deleted reservation '%v'", reservation.Id)
+					}
+				}
+			}
+		}
+		fmt.Printf("Task '%v' finished @ %v \n", "checkForExpiredReservations", time.Now())
+	}
+}
