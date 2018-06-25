22f393227181b3ffb34cb5ee7c255ebf1b5700e9
loehnertz <jakob2102@gmail.com>
2018-06-25 19:16:52 +0200

    Added a new custom error for the upcoming API-Gateway

--- commons/errors.go
+++ commons/errors.go
@@ -7,3 +7,4 @@ const FoundRowsErrorSubstring = "destination arguments in Scan,"
 
 // Custom errors
 var UnknownError = errors.New("an unknown error ocurred")
+var NotAuthorizedError = errors.New("you are not authorized to access this path")
