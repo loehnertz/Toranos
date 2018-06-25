145e433f9ab5ef3ce81a9efbab91e37931993ec6
loehnertz <jakob2102@gmail.com>
2018-06-26 00:59:17 +0200

    Implemented some helper functions regarding the requests towards the API-Gateway

--- api-gateway/requests.go
+++ api-gateway/requests.go
@@ -1,6 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/loehnertz/toranos/commons"
+	"github.com/micro/go-log"
+	"io"
+	"net/http"
+)
+
 type getAuthTokenRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type createBookingRequest struct {
+	VehicleId string `json:"vehicle"`
+}
+
+func deserialize(target interface{}, body io.ReadCloser) (deserialized interface{}, err error) {
+	decoder := json.NewDecoder(body)
+	decodeError := decoder.Decode(&target)
+	if decodeError != nil {
+		log.Log(decodeError)
+		return nil, commons.UnknownError
+	} else {
+		return target, nil
+	}
+}
+
+func respondWithJson(w *http.ResponseWriter, r interface{}) {
+	jsonBytes, marshalError := json.Marshal(r)
+	if marshalError != nil {
+		log.Log(marshalError)
+		(*w).Write([]byte(commons.UnknownError.Error()))
+	}
+
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).Write(jsonBytes)
 }
