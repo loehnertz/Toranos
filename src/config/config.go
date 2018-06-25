7082e51fe909e6d713d0678c37341c949dce2589
loehnertz <jakob2102@gmail.com>
2018-06-25 14:56:08 +0200

    Added some more constants to the config

--- src/config/config.go
+++ src/config/config.go
@@ -4,13 +4,18 @@ package config
 const FleetControllerName = "fleet-controller"
 const FleetMonitorName = "fleet-monitor"
 const TelemetryName = "telemetry"
+const UserManagementName = "user-management"
 
-// Booking settings
+// FleetController settings
 const StatusError = 0
 const StatusReserved = 1
 const StatusDriving = 2
 const StatusDone = 3
 const ReservationTimeInSeconds = 900
+
+// UserManagement settings
+const TokenSecretTableKey = "token_secret"
+const AudienceKeyCustomer = "customer"
 
 // FleetMonitor cron intervals
 const CheckForExpiredReservationsInterval = "0 */1 * * * *"
