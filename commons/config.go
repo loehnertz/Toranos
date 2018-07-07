718e278b5c789e40abe4be378af7383f10b9db98
loehnertz <jakob2102@gmail.com>
2018-07-07 20:01:48 +0200

    Implemented common helper functions to use the K/V store of 'Consul' as a configuration manager

---
+++ commons/config.go
@@ -0,0 +1,30 @@
+package commons
+
+import (
+	"errors"
+	"fmt"
+	"github.com/micro/go-config"
+	"github.com/micro/go-config/source/consul"
+)
+
+func GetConfigByPath(path ...string) string {
+	conf := initConfig()
+	return conf.Get(path...).String(generateNewConfigError(path...).Error())
+}
+
+func initConfig() config.Config {
+	// Create Consul as a source
+	consulSource := consul.NewSource(consul.WithPrefix("/"))
+
+	// Create new config
+	conf := config.NewConfig()
+
+	// Load file source
+	conf.Load(consulSource)
+
+	return conf
+}
+
+func generateNewConfigError(path ...string) error {
+	return errors.New(fmt.Sprintf("No config could be found for path '%v'", path))
+}
