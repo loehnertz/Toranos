1ac6614117e166f96f191c5992e2f76499d8e5ec
loehnertz <jakob2102@gmail.com>
2018-07-08 19:16:18 +0200

    Added the configuration to the 'statistics' service

--- services/statistics/main.go
+++ services/statistics/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
-	"github.com/loehnertz/toranos/config"
+	"github.com/loehnertz/toranos/common"
 	"github.com/loehnertz/toranos/services/fleet-controller/proto"
 	"github.com/loehnertz/toranos/services/statistics/proto"
+	"github.com/micro/go-config"
 	"github.com/micro/go-micro"
 	"time"
 )
 
+var conf config.Config
 var service micro.Service
 var fleetControllerService fleet_controller.FleetControllerService
 
@@ -25,9 +27,12 @@ func (st *Statistics) RetrieveBookings(ctx context.Context, req *statistics.Retr
 }
 
 func main() {
+	// Initialize the configuration
+	conf = common.InitConfig()
+
 	// Create the service
 	service = micro.NewService(
-		micro.Name(config.StatisticsName),
+		micro.Name(common.GetConfigStringByPath(conf, "service-names", "statistics")),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*10),
 	)
@@ -37,7 +42,10 @@ func main() {
 	statistics.RegisterStatisticsHandler(service.Server(), new(Statistics))
 
 	// Initialize the service clients
-	fleetControllerService = fleet_controller.NewFleetControllerService(config.FleetControllerName, service.Client())
+	fleetControllerService = fleet_controller.NewFleetControllerService(
+		common.GetConfigStringByPath(conf, "service-names", "fleet-controller"),
+		service.Client(),
+	)
 
 	// Run the server
 	if err := service.Run(); err != nil {
